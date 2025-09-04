8a65d7653c3da406be252f5b39ed447d80dc66ce
Ali Shirazi <afashirazi@proton.me>
2025-09-04 12:30:27 +0300

    naive read to buf, 8.5s to read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs, io::Read, path::Path, time::{self, Duration, Instant}};
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+    let path = Path::new("./resources/measurements.txt");
+    let mut f = fs::File::open(path).unwrap();
+    let mut buf = vec![];
+
+    let size = f.read_to_end(&mut buf).unwrap();
+    println!("size of buffer: {size}");
+    println!("time taken to read: {}", Instant::now().duration_since(start).as_nanos());
+
 }
