24a2e0af7472ad18284f4ab7d39189a8e05649e4
Ali Shirazi <afashirazi@proton.me>
2025-09-05 15:54:17 +0300

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ fn main() {
     );
 
     let map = buf.split('\n')
+    .filter(|el| !el.trim().is_empty())
     .map(|line| {
         let mut split = line.split(';');
         let name = split.next().unwrap();
-        let temp = split.next().map(|num| num.parse::<f64>().unwrap()).unwrap();
+        let temp = split.next().map(|num| num.parse::<f64>().unwrap()).expect(line);
         (name, temp)
     })
     .fold(HashMap::new(), |mut map, (name, temp)| {
@@ -27,7 +28,7 @@ fn main() {
             let (omin, omean, omax, on) = map.get(name).unwrap();
             let nn = on + 1;
             let nmin = if temp < *omin { temp } else {*omin};
-            let nmean = (omean + temp) / nn as f64;
+            let nmean = (omean * *on as f64 + temp) / nn as f64;
             let nmax = if temp > *omax { temp } else { *omax };
             map.insert(name, (nmin, nmean, nmax, nn));
         } else {
@@ -36,10 +37,15 @@ fn main() {
         map
     });
 
+    println!(
+        "time taken to map: {}",
+        Instant::now().duration_since(start).as_nanos()
+    );
+
     let mut output = fs::File::create_new(Path::new("./resources/results.txt")).unwrap();
     map.iter().for_each(
         |(name, (min, mean, max, _))| {
-            output.write(format!("{}={}/{}/{}", name, min, mean, max).as_bytes()).unwrap();
+            output.write(format!("{}={}/{}/{}\n", name, min, mean, max).as_bytes()).unwrap();
         }
     );
 }
